6e828419a1fc510f6d7602b84d574d9c67a58b75
Anson <git@anson-cheung.com>
2020-04-09 18:33:11 +0800

    Remove debug statement

--- src/dive_plan/mod.rs
+++ src/dive_plan/mod.rs
@@ -13,7 +13,5 @@ fn equivalent_narcotic_depth(depth: usize, gas: &Gas) -> usize { // Returns a de
 
 fn gas_in_ppo2_range(depth: usize, min: f64, max: f64, gas: &Gas, metres_per_bar: f64) -> bool { // Checks if gas is in ppo2 range
     let gas_ppo2 = partial_pressure(depth, gas.fr_o2(), 10.0);
-    println!("{}m, {} max ppO2, {} calculated ppO2, {:?}, result: {}", depth, max, gas_ppo2, gas, gas_ppo2 >= min && gas_ppo2 <= max);
-
     gas_ppo2 >= min && gas_ppo2 <= max
 }
