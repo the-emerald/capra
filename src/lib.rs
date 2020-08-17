4c93691c4bf440d07e61fa14d7193b48e981d968
Anson <git@anson-cheung.com>
2020-08-17 16:27:11 +0800

    Update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,59 @@
-//! Dive planning library with support for open circuit dives
+//! Dive planning library
+//! # Quick example
+//! A quick example showing how to use this library:
+//! ```
+//! use time::Duration;
+//! use capra_core::{common, deco};
+//! use capra::DivePlan;
+//! use capra::modes::OpenCircuit;
+//!
+//! fn main() {
+//!     // Make a new gas
+//!     let air = common::Gas::new(21, 0, 79).unwrap();
+//!
+//!     // Make a new ZHL16 decompression model
+//!     let zhl16 = deco::zhl16::ZHL16::new_by_variant(
+//!         deco::Tissue::default(),
+//!         50,
+//!         70,
+//!         deco::zhl16::Variant::B,
+//!     );
+//!
+//!     // Make a dive segment
+//!     let dive_segment = common::DiveSegment::new(
+//!         common::SegmentType::DiveSegment,
+//!         45,
+//!         45,
+//!         Duration::minutes(60),
+//!         -10,
+//!         20,
+//!     )
+//!     .unwrap();
+//!
+//!     let deco_gases = vec![(air, None)];
+//!     let segments = vec![(dive_segment, air)];
+//!     let open_circuit = OpenCircuit::new(
+//!         zhl16,
+//!         &deco_gases,
+//!         &segments,
+//!         -10,
+//!         20,
+//!         common::DENSITY_SALTWATER,
+//!         20,
+//!         15,
+//!     );
+//!
+//!     let results = open_circuit.plan();
+//!
+//!     for (segment, gas) in results.total_segments() {
+//!         println!("{:?}, {:?}", segment, gas);
+//!     }
+//!
+//!     for (gas, qty) in results.gas_used() {
+//!         println!("{:?}: {}", gas, qty);
+//!     }
+//! }
+//! ```
 
 pub mod dive_result;
 pub mod modes;
