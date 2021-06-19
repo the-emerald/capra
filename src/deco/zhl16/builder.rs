753938993ecc250bab9c09550cb48cf7e2295b3f
Anson <git@anson-cheung.com>
2021-06-19 21:55:31 +0800

    Fix deco increment

--- src/deco/zhl16/builder.rs
+++ src/deco/zhl16/builder.rs
@@ -79,7 +79,7 @@ impl ZHL16Builder {
             diver_depth: self.diver_depth,
             first_deco_depth: self.first_deco_depth,
             gf: self.gradient_factor,
-            deco_increment: self.deco_increment
+            deco_increment: self.deco_increment,
         }
     }
 }
